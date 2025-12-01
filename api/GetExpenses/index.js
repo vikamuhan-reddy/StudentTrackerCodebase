9d35219d2add1acae4f7de55c1be023a59692820
Vikamuhan <144928933+vikamuhan-reddy@users.noreply.github.com>
2025-12-01 16:53:08 +0530

    Update index.js

--- api/GetExpenses/index.js
+++ api/GetExpenses/index.js
@@ -1,1 +1,16 @@
+const { TableClient } = require("@azure/data-tables");
 
+module.exports = async function (context, req) {
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+    const tableClient = TableClient.fromConnectionString(connectionString, "ExpenseTable");
+
+    let expenses = [];
+    for await (const entity of tableClient.listEntities()) {
+        expenses.push(entity);
+    }
+
+    return {
+        status: 200,
+        body: expenses
+    };
+};
