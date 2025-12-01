70cce57149bb9b7a97e3d7f53fe490a21d1a9992
Vikamuhan <144928933+vikamuhan-reddy@users.noreply.github.com>
2025-12-01 16:53:59 +0530

    Update index.js

--- api/DeleteExpense/index.js
+++ api/DeleteExpense/index.js
@@ -1,1 +1,15 @@
+const { TableClient } = require("@azure/data-tables");
 
+module.exports = async function (context, req) {
+    const { rowKey } = req.query;
+
+    const connectionString = process.env.STORAGE_CONNECTION_STRING;
+    const tableClient = TableClient.fromConnectionString(connectionString, "ExpenseTable");
+
+    await tableClient.deleteEntity("expense", rowKey);
+
+    return {
+        status: 200,
+        body: { message: "Expense deleted!" }
+    };
+};
